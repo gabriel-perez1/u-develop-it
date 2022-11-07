eee04b5fcb788dc77f7df04cc9b3ad1f88dfad55
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2022-11-07 18:05:52 -0400

    create the get routes

--- server.js
+++ server.js
@@ -25,13 +25,44 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
 	console.log(rows);
 });*/
 
-// GET a single candidate
-/*db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
-	if (err) {
-		console.log(err);
-	}
-	console.log(row)
-});*/
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+});
+
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+	const sql = `SELECT * FROM candidates`;
+
+	db.query(sql, (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: 'success',
+			data: rows
+		});
+	});
+});
+
+// get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+	const sql = `SELECT * FROM candidates WHERE id = ?`;
+	const params = [req.params.id];
+
+	db.query(sql, params, (err, row) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: 'success',
+			data: row
+		});
+	});
+});
 
 /* delete a candidate
 db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
@@ -42,16 +73,16 @@ db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
 });*/
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+/*onst sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 							VALUES (?,?,?,?)`;
 const params = [1, 'Ronald', 'Firbank', 1];
 
-/*db.query(sql, params, (err, result) => {
+db.query(sql, params, (err, result) => {
 	if (err) {
 		console.log(err);
 	}
 	console.log(result);
-});
+}); */
 
 // default response for any other request (not found)
 app.use((req, res) => {
@@ -60,4 +91,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
-});*/
+});
