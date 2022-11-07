479be020a1eac67a0edf23f885273eb27820ee3e
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2022-11-07 16:48:08 -0400

    connect to mysql database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// your MySQL username
+		user: 'root',
+		// your MySQL password
+		password: 'MySQL_Root_Password1!',
+		database: 'election'
+	},
+	console.log('Connected to the election database')
+);
 
 // default response for any other request (not found)
 app.use((req, res) => {
